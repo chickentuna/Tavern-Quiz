e66923c221d242941a74b04263bf4f2b0b2fe8e0
Julien Poulton <julien.poulton@gmail.com>
2017-02-02 14:10:40 +0100

    meh

--- app.js
+++ app.js
@@ -51,11 +51,15 @@ var cardImg;
 function getCallback(index, list) {
 	return function(data) {
 		var all = processSynonyms(data.data);
+		var truth = list[index][0]
 		list[index] = all;
+		all.push(truth);
 	};
 }
 
 function launch() {
+	$('#card img').attr('src', '');
+
 	var category = choose(categories);
 	var n = randInt(allCards.en[category].length)
 	var card = choose(allCards.en[category]);
@@ -77,19 +81,60 @@ function launch() {
 		possibles[i] = [decontructed[i]];
 
 		if (ignore.indexOf(word) == -1) {
-			promises.push(retrieveSynonymns(word).then(getCallback(i, possibles));
+			promises.push(retrieveSynonymns(word).then(getCallback(i, possibles)));
 		}
+
 	}
+
+	//TODO: if possessive, pick only nouns
 
 	axios.all(promises).then(function() {
 		var propositions = [realName];	
-		$('#propositions').empty();
-		$('#propositions').append('<li><button onclick="launch()">'+realName+'</button></li>')
+		
 		$('#card img').attr('src', cardImg);
-		console.log(possibles);
+		if (possibles.every(v => v.length == 1)) {
+			launch();
+			return;
+		}
+
+		var n = 6;
+		for (var i = 0; i < n; ++i) {
+			var reconstructed = possibles.map(arr => choose(arr)).map(str => str[0].toUpperCase() + str.slice(1));
+			var name = reconstructed.join(' ');
+			if (propositions.indexOf(name) == -1) {
+				propositions.push(name);
+			}
+			
+		}
+
+		shuffle(propositions);
+		$('#propositions').empty();
+		for (var i = 0; i < propositions.length; ++i) {
+			if (propositions[i] == realName) {
+				$('#propositions').append('<li><button onclick="correct()">' + propositions[i] + '</button></li>')	
+			} else {
+				$('#propositions').append('<li><button onclick="wrong()">' + propositions[i] + '</button></li>')
+			}
+		}
 	});
 
 	
+}
+
+function correct() {
+	alert('Correct');
+	launch();
+}
+function wrong() {
+	alert('Wrong');
+	launch();
+}
+
+function shuffle(a) {
+    for (let i = a.length; i; i--) {
+        let j = Math.floor(Math.random() * i);
+        [a[i - 1], a[j]] = [a[j], a[i - 1]];
+    }
 }
 
 var key = 'TeR4qHrTKo2ruvyPMlHc'
